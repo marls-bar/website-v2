58d8ee0c91b6d231ac13d9591d9d465eef919151
Aidan Marley <aidanmarley@Aidans-MBP.modem>
2022-12-01 16:19:28 +1100

    Navbar tailwind not working but bootstrap is

---
+++ website-v2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import "./index.css";
+import './App.css';
+import Navbar from "./Navigation/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
