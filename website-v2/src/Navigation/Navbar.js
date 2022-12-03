091469c7cde5e3a8ed75bed3f6c0359a3a9efff0
Aidan Marley <aidanmarley@Aidans-MBP.modem>
2022-12-03 11:17:54 +1100

    issue with routing

--- website-v2/src/Navigation/Navbar.js
+++ website-v2/src/Navigation/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <ul class="nav justify-content-center">
             <li class="nav-item">
-                <a class="nav-link" href="#">About me</a>
+                <a class="nav-link" href="/home">About me</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Projects</a>
+                <a class="nav-link" href="/projects">Projects</a>
             </li>
         </ul>
   )
---
+++ website-v2/src/Pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Projects() {
+    return (
+      <>
+        <h2>TEST</h2>
+      </>
+    );
+}
+
+export default Projects;
